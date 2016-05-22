040fea30813b1683d7cde6d0cdcd1b94b4eafbb0
bill <langhoffWilliam@gmail.com>
2016-05-22 14:39:17 -0500

    passing input/output number tests

--- neat/genome.py
+++ neat/genome.py
@@ -3,4 +3,9 @@ from .genes import NodeGene, LinkGene
 class Genome:
 
     def __init__(self, *, n_inputs, n_outputs):
-        pass
+        if n_inputs < 1:
+            raise ValueError("Genome needs positive number of inputs")
+        if n_outputs < 1:
+            raise ValueError("Genome needs positive number of outputs")
+        self.n_inputs = n_inputs
+        self.n_outputs = n_outputs
